d28fb0812ab3d120523881e40602341ff847a92b
jkanester1991 <jkanester@gmail.com>
2026-01-21 11:08:24 -0800

    added basic form handling

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML =
+    "The fog comes on little cat feet. It sits looking over harbor and city on silent haunches and then moves on";
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
